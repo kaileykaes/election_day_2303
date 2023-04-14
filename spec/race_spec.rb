a24ed8ad22a5fd827ba26bbd5a2ebc3643baa9da
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-14 09:40:08 -0600

    Fix a few oversights

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -18,11 +18,11 @@ RSpec.describe Race do
 
   describe 'candidate registration' do 
     it '#register_candidate!' do 
-      candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
-      expect(candidate1.class).to be_a candidate
+      candidate1 = @race.register_candidate!({name: "Diana D", party: :democrat})
+      expect(candidate1.class).to eq(Candidate)
       expect(candidate1.name).to eq('Diana D')
       expect(candidate1.party).to eq(:democrat)
-      candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+      candidate2 = @race.register_candidate!({name: "Roberto R", party: :republican})
       expect(@race.candidates).to eq([candidate1, candidate2])
     end
   end
