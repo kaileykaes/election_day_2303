f130d64b280e869983ef4cba91e6d089e886ed3d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-14 09:15:13 -0600

    Test: #initialize & #vote_for!

---
+++ spec/candidate_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe Candidate do
+  before(:each) do
+    @diana = Candidate.new({name: "Diana D", party: :democrat})
+  end
+
+  describe '#initialize' do 
+    it 'exists' do
+      expect(@diana).to be_a Candidate
+    end
+
+    it 'has attributes' do 
+      expect(@diana.name).to eq('Diana D')
+      expect(@diana.party).to eq(:democrat)
+      expect(@diana.votes).to eq(0)
+    end
+  end
+
+  describe 'voting' do 
+    it '#vote_for!' do 
+      @diana.vote_for! 
+      @diana.vote_for! 
+      @diana.vote_for! 
+      expect(@diana.votes).to eq(3)
+      @diana.vote_for! 
+      expect(@diana.votes).to eq(4)
+    end
+  end
+end
